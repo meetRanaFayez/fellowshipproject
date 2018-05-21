6f59ce1993d712b6812be63c5b505459fc58cc9b
Fayez, Rana <ranafayez@gmail.com>
2018-05-21 13:19:41 -0400

    mapbox fixes

--- main.js
+++ main.js
@@ -6,6 +6,6 @@ L.tileLayer('https://api.mapbox.com/styles/v1/ranaransom/cjhgetl7n4dy82smjwjbdhh
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox.streets',
-    accessToken: 'your.mapbox.access.token'
-}).addTo(mymap);
+    accessToken: 'pk.eyJ1IjoicmFuYXJhbnNvbSIsImEiOiJjamduejhiZGQwa2lrMzNyMXNsbmdrM3B1In0.3YYL35-tBzpDjgxiG76-LQ'
+}).addTo(map);
 
